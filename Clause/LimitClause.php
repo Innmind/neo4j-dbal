9a4f08c194fd3b87311f1282a5e4c74b1c8579fe
Baptiste Langlade <baptouuuu@gmail.com>
2016-02-20 17:33:24 +0100

    add limit clause

---
+++ Clause/LimitClause.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Neo4j\DBAL\Clause;
+
+use Innmind\Neo4j\DBAL\ClauseInterface;
+
+class LimitClause implements ClauseInterface
+{
+    const IDENTIFIER = 'LIMIT';
+
+    private $cypher;
+
+    public function __construct(string $cypher)
+    {
+        $this->cypher = $cypher;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function identifier(): string
+    {
+        return self::IDENTIFIER;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __toString(): string
+    {
+        return $this->cypher;
+    }
+}
