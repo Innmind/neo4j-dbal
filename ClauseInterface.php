0058d46cd305fbe4d13138602d5d33093828e47d
Baptiste Langlade <baptouuuu@gmail.com>
2016-02-20 15:58:25 +0100

    add interface to represent a cypher clause

---
+++ ClauseInterface.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Neo4j\DBAL;
+
+interface ClauseInterface
+{
+    /**
+     * Return the line identifier of the clause (ie: MATCH, WHERE, SET, etc...)
+     *
+     * @return string
+     */
+    public function identifier(): string;
+
+    /**
+     * Return the cypher representation of the clause
+     *
+     * @return string
+     */
+    public function __toString(): string;
+}
